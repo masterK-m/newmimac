a05a3c3e18bb13efff9b503acf866358cc4d418d
hagrunes <iruel@hanmail.net>
2019-01-17 18:06:36 +0900

    app.js 커밋

---
+++ test_hs/nodebird/app.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const path = require('path');
+const session = require('express-session');
+const flash = require('connect-flash');
