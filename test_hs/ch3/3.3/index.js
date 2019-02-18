d0712c76b760e83379bcf837c394036e38e6cb63
hagrune <iruel@hanmail.net>
2019-02-18 13:55:11 +0900

    test

--- test_hs/ch3/3.3/index.js
+++ test_hs/ch3/3.3/index.js
@@ -1,5 +1,22 @@
-const { odd, even } = require('./var');
+const {
+  odd,
+  even
+} = require('./var'); //비구조화 할당
 const checkNumber = require('./func');
+
+/*
+//구 문법
+const variable = require('./var');
+console.log(variable.odd);
+console.log(variable.even);
+
+module.exports = {
+  odd : odd,
+  even : even
+}
+
+
+*/
 
 function checkStringOddOrEven(str) {
   if (str.length % 2) { // 홀수면
