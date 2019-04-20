9906667e68f1c8bb8db3b3f60cade438c69909cf
hagrunes <iruel@hanmail.net>
2019-04-21 01:41:48 +0900

    test

---
+++ test_hs/reactTest/lecture/WordRelay.jsx
@@ -0,0 +1,17 @@
+const React = require('react');
+const {
+    Component
+} = React;
+
+class WordRelay extends Component {
+    state = {
+        text: 'Hello, webPack'
+    }
+
+    render() {
+        return <h1> {this.state.text} </h1>;
+    }
+
+};
+
+module.exports = WordRelay;
