f23e87341c12d0d5681c3d2b26eabe89255654a0
hagrune <iruel@hanmail.net>
2019-04-25 16:10:14 +0900

    ㅌ스트

--- test_hs/reactTest/ResponseCheck/ResponseCheck.jsx
+++ test_hs/reactTest/ResponseCheck/ResponseCheck.jsx
@@ -7,32 +7,47 @@ class ResponseCheck extends Component {
         result : []
     };
 
+    timeout;
+    startTime;
+    endTime;
+
     onClickScreen = () => {
         const {state, message, result} = this.state;
         if(state === "waiting") {
-
             this.setState({
                 state : 'ready',
                 message : '초록색이 되면 클릭하세요.'
             });
-            setTimeout( () => {
+
+            this.timeout =  setTimeout( () => {
                 this.setState({
                     state : 'now',
                     message : '지금클릭'
                 })
-            }, Math.floor(Math.random() * 1000 + 2000));
+                this.startTime =  new Date();
+            }, Math.floor(Math.random() * 1000 ) + 2000);
 
         } else if (state === 'ready') {
-
+            clearTimeout(this.timeout);
+            this.setState({
+                state : 'waiting',
+                message : '급하다 급해'
+            })
         } else if (state === 'now') {
-
+            this.endTime = new Date();
+            this.setState( (prevState) => {
+                return {
+                    state : 'waiting',
+                    message : '클릭해서 시작하세요.',
+                    result : [...prevState.result, this.endTime - this.statTime]
+                };
+            });
         }
-
-    }
+    };
 
     renderAverage = () => {
         const {result} = this.state;
-        return result.length === 0 ? null : <div>평균시간 : {result.reduce((a ,c) => a + c / result.length )}ms</div>
+        return result.length === 0 ? null : <div>평균시간 : {result.reduce((a ,c) => a + c) / result.length } ms</div>
     }
 
     render() {
@@ -49,7 +64,7 @@ class ResponseCheck extends Component {
                 {
                 /*result.length === 0 ? null : <div>평균시간 : {result.reduce((a ,c) => a + c / result.length )}ms</div>                */
                 }
-                {this.renderAverage}
+                {this.renderAverage()}
             </>
         );
     }
